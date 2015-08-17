fd3e813d39cfdcb3ad349bfec1f115359090d797
ocean89 <martin.grantz@gmail.com>
2015-08-17 11:13:54 +0200

    Changed config files and removed unused things
    Signed-off-by: ocean89 <martin.grantz@gmail.com>

--- www/ios/diagnostic.js
+++ www/ios/diagnostic.js
@@ -61,58 +61,5 @@ Diagnostic.prototype.isLocationAuthorized = function(successCallback, errorCallb
 						[]);
 };
 
-/**
- * Checks if exists Wi-Fi connection.
- *
- * @param successCallback	The callback which will be called when diagnostic of Wi-Fi is successful.
- * 							This callback function have a boolean param with the diagnostic result.
- * @param errorCallback		The callback which will be called when diagnostic of Wi-Fi encounters an error.
- * 							This callback function have a string param with the error.
- */
-
-Diagnostic.prototype.isWifiEnabled = function(successCallback, errorCallback) {
-	return cordova.exec(successCallback,
-						errorCallback,
-						'Diagnostic',
-						'isWifiEnabled',
-						[]);
-};
-
-/**
- * Checks if exists camera.
- *
- * @param successCallback	The callback which will be called when diagnostic of camera is successful.
- * 							This callback function have a boolean param with the diagnostic result.
- * @param errorCallback		The callback which will be called when diagnostic of camera encounters an error.
- * 							This callback function have a string param with the error.
- */
-
-
-Diagnostic.prototype.isCameraEnabled = function(successCallback, errorCallback) {
-	return cordova.exec(successCallback,
-						errorCallback,
-						'Diagnostic',
-						'isCameraEnabled',
-						[]);
-};
-
-/**
- * Checks if Bluetooth is enabled
- *
- * @param successCallback	The callback which will be called when diagnostic of Bluetooth is successful.
- * 							This callback function have a boolean param with the diagnostic result.
- * @param errorCallback		The callback which will be called when diagnostic of Bluetooth encounters an error.
- * 							This callback function have a string param with the error.
- */
-
-
-Diagnostic.prototype.isBluetoothEnabled = function(successCallback, errorCallback) {
-	return cordova.exec(successCallback,
-		errorCallback,
-		'Diagnostic',
-		'isBluetoothEnabled',
-		[]);
-};
-
 module.exports = new Diagnostic();
 
