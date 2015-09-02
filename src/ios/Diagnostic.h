076493868bedae1176828d8b5670dfb880eba4e5
David <david.barre@shippeo.com>
2015-09-02 15:56:09 +0200

    64bit arm ios support & more cleanup

--- src/ios/Diagnostic.h
+++ src/ios/Diagnostic.h
@@ -6,20 +6,12 @@
 //
 
 #import <Cordova/CDV.h>
-#import <CoreBluetooth/CoreBluetooth.h>
 
-
-@interface Diagnostic : CDVPlugin <CBCentralManagerDelegate>
-
-    @property (nonatomic, retain) CBCentralManager *bluetoothManager;
-    @property (nonatomic) BOOL bluetoothEnabled;
+@interface Diagnostic : CDVPlugin
 
 - (void) isLocationEnabled: (CDVInvokedUrlCommand*)command;
 - (void) isLocationEnabledSetting: (CDVInvokedUrlCommand*)command;
 - (void) switchToLocationSettings: (CDVInvokedUrlCommand*)command;
 - (void) isLocationAuthorized: (CDVInvokedUrlCommand*)command;
-- (void) isWifiEnabled: (CDVInvokedUrlCommand*)command;
-- (void) isCameraEnabled: (CDVInvokedUrlCommand*)command;
-- (void) isBluetoothEnabled: (CDVInvokedUrlCommand*)command;
 
 @end
